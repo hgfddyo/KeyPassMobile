2db298cf3bd182d1afc8119885cc55e27e07982e
artemand3 <hgfddyo@gmail.com>
2021-08-16 17:27:57 +1000

    Login component preparation

---
+++ KeyPass/src/LoginComponent/login_component.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+class LoginComponent extends React.Component {
+    render() {
+        return (
+            <Text>Привет, Мир</Text>
+        );
+    }
+}
+export default LoginComponent;
