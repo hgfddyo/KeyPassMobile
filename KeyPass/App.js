d90b32a9ae5ce3d3e339091f0339cc6c1960c2c0
artemand3 <hgfddyo@gmail.com>
2021-08-17 15:29:46 +1000

    config changed

--- KeyPass/App.js
+++ KeyPass/App.js
@@ -4,18 +4,16 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginComponent from "./src/LoginComponent/login_component";
 
-
-
 const Drawer = createDrawerNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen name="Login" component={LoginComponent} />
-            </Drawer.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Login" component={LoginComponent} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
