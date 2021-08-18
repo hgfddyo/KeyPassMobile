228d6d128fe5ede054466b5498364d6f14760a86
artemand3 <hgfddyo@gmail.com>
2021-08-19 00:14:06 +1000

    scripts for creating tables

--- KeyPass/App.js
+++ KeyPass/App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginComponent from "./src/LoginComponent/login_component";
@@ -7,6 +6,7 @@ import DBUtils from "./src/DBUtils/DBUtils";
 
 const Drawer = createDrawerNavigator();
 let db = new DBUtils();
+db.createTables();
 
 function App() {
   return (
--- KeyPass/src/DBUtils/DBUtils.js
+++ KeyPass/src/DBUtils/DBUtils.js
@@ -7,4 +7,27 @@ export default class DBUtils {
     this.db = openDatabase({ name: 'KeyRingDB.db', location: 'default' });
   }
 
+  createTables(){
+    this.db.transaction(tx => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS Users(" +
+        "Id_user	INTEGER NOT NULL UNIQUE, " +
+        "Username	TEXT NOT NULL UNIQUE, " +
+        "Password	TEXT NOT NULL, " +
+        "PRIMARY KEY(Id_user AUTOINCREMENT)); "
+      );
+    });
+    this.db.transaction(tx => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS Keys(" +
+        "Id_key	INTEGER NOT NULL UNIQUE, " +
+        "Id_user INTEGER NOT NULL," +
+        "Context TEXT, " +
+        "Login	TEXT NOT NULL, " +
+        "Password	TEXT NOT NULL, " +
+        "FOREIGN KEY(Id_user) REFERENCES Users(Id_user)," +
+        "PRIMARY KEY(Id_key AUTOINCREMENT));"
+      );
+    });
+  }
 }
