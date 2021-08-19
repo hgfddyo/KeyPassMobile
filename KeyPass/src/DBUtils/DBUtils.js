65851ace5fdbfade3d8ae08e3e1b4246bf14e379
artemand3 <hgfddyo@gmail.com>
2021-08-20 04:44:30 +1000

    another fix

--- KeyPass/src/DBUtils/DBUtils.js
+++ KeyPass/src/DBUtils/DBUtils.js
@@ -1,7 +1,6 @@
 import { openDatabase } from 'react-native-sqlite-storage';
 
 export default class DBUtils {
-  db: any;
 
   constructor() {
     this.db = openDatabase({ name: 'KeyRingDB.db', location: 'default' });
