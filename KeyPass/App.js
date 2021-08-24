76144a041ac971ea55cdd18a2121957d5cc1f52f
hgfddyo <hgfddyo@gmail.com>
2021-08-24 23:07:17 +1000

    installed and included rn-paper library

--- KeyPass/App.js
+++ KeyPass/App.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import LoginComponent from './src/LoginComponent/login_component';
 import RegistrationComponent from './src/RegistrationComponent/registration_component';
 import DBUtils from './src/DBUtils/DBUtils';
+import {Provider as PaperProvider} from 'react-native-paper';
 
 const Drawer = createDrawerNavigator();
 let db = new DBUtils();
@@ -11,12 +12,17 @@ db.createTables();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Login" component={LoginComponent} />
-        <Drawer.Screen name="Registration" component={RegistrationComponent} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Login" component={LoginComponent} />
+          <Drawer.Screen
+            name="Registration"
+            component={RegistrationComponent}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
