4a517d5dfc5f24c9fd61679c85a24eb3affe3bf0
hgfddyo <hgfddyo@gmail.com>
2021-09-14 22:15:10 +0900

    implemented search logic for header input

--- KeyPass/src/KeysTableComponent/styles.js
+++ KeyPass/src/KeysTableComponent/styles.js
@@ -1,4 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-let styles = StyleSheet.create();
+let styles = StyleSheet.create({
+  headerSearchInput: {
+    width: 150,
+    height: 40,
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  headerRightButton: {
+    marginRight: 20,
+  },
+});
 export default styles;
