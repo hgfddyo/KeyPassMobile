3e7efddd55fb71b53d956a91ecf78664c823ccc7
ivansasha1999 <ivansasha1999@gmail.ru>
2021-10-13 18:52:53 +1000

    RefactoringButtons

--- KeyPass/src/UpdateKeyComponent/updatekey_component.js
+++ KeyPass/src/UpdateKeyComponent/updatekey_component.js
@@ -167,7 +167,7 @@ class UpdateKeyComponent extends React.Component {
           style={styles.fab}
           small
           icon="chevron-left"
-          label="back"
+          label="cancel"
           color={'#00C4B4'}
           onPress={() => {
             this.props.navigation.goBack();
